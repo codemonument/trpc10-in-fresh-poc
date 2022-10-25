dae634e415cf6450fb4ce1ae475a5e858719b380
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 01:21:13 +0200

    write server to export trpcExpressMiddleware

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,7 +1,19 @@
-import { initTRPC } from "@trpc/server";
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+import {
+  CreateExpressContextOptions,
+  createExpressMiddleware,
+} from "@trpc/server/adapters/express";
 import { z } from "zod";
 
-const t = initTRPC.create();
+// Initialize a context for the server
+function createContext(opts: CreateExpressContextOptions) {
+  return {};
+}
+
+// Get the context type
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
 
 const appRouter = t.router({
   hello: t.procedure
@@ -15,3 +27,8 @@ const appRouter = t.router({
 // to avoid accidentally importing your API
 // into client-side code
 export type AppRouter = typeof appRouter;
+
+export const trpcExpressMiddleware = createExpressMiddleware({
+  router: appRouter,
+  createContext,
+});

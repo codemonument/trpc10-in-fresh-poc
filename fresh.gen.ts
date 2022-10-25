a28fa7cdd2c956a626f7d3c18076313f2acf9303
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 01:11:25 +0200

    regenerate fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,17 +5,21 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $2 from "./routes/api/trpc.ts";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
+import * as $$1 from "./islands/TrpcPlayground.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/api/trpc.ts": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
+    "./islands/TrpcPlayground.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,

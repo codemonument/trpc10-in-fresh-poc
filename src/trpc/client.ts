a00c2cb52cc6edf6a6f1d17fc58d3dca37d49cfe
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 18:41:46 +0200

    replace custom check for globalscope?.location with IS_BROWSER from fresh runtime

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -1,13 +1,14 @@
-// @filename: client.ts
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
 import type { AppRouter } from "./server.ts";
 
 /**
- * This guard check for the availability of 'location' is necessary,
- * since location is not defined in global scope on server and crashes on deno deploy.
+ * This guard check for IS_BROWSER is necessary,
+ * since `location` is not defined in global scope on server and crashes on deno deploy.
  */
 let host;
-if (globalThis.location) {
+if (IS_BROWSER) {
   console.log("Origin: ", location?.origin);
   host = location?.origin;
 }

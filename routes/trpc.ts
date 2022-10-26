1fa21a630872510872aee2f2e8080d9666c405ed
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 13:18:28 +0200

    try touse a simple promise to adapt trpc express middleware to fresh request handler

--- routes/trpc.ts
+++ routes/trpc.ts
@@ -1,11 +1,21 @@
 import { HandlerContext } from "$fresh/server.ts";
 import { trpcExpressMiddleware } from "@/src/trpc/server.ts";
+import { resolve } from "https://deno.land/std@0.150.0/path/win32.ts";
 
-export const handler = (_req: Request, _ctx: HandlerContext): Response => {
-  trpcExpressMiddleware.apply(_req, {
-    // TODO: How to mock response object correctly?
-    send: () => {
+export const handler = async (
+  _req: Request,
+  _ctx: HandlerContext,
+): Promise<Response> => {
+  const expressHandlerPromise = await new Promise<Response>(
+    (resolve, reject) => {
+      trpcExpressMiddleware.apply(_req, {
+        // TODO: How to mock response object correctly?
+        send: (param: string) => {
+          resolve(new Response(param));
+        },
+      });
     },
-  });
-  return new Response("Not yet defined");
+  );
+
+  return expressHandlerPromise;
 };

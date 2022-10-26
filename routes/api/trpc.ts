39e7af4539d80d690c2ac6add1ad28eeb9f0885d
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 11:54:48 +0200

    tried to implement a trpc api route

--- routes/api/trpc.ts
+++ routes/api/trpc.ts
@@ -1,5 +1,13 @@
 import { HandlerContext } from "$fresh/server.ts";
+import { trpcExpressMiddleware } from "@/src/trpc/server.ts";
+
+const middlewareType = typeof trpcExpressMiddleware;
 
 export const handler = (_req: Request, _ctx: HandlerContext): Response => {
+  trpcExpressMiddleware.apply(_req, {
+    // TODO: How to mock response object correctly?
+    send: () => {
+    },
+  });
   return new Response("Not yet defined");
 };

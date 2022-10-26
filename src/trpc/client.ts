693c632d3b3b79ef0695d22dec723db7772eee0f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 15:50:11 +0200

    make trpc origin agnistic to running locally and on deno deploy

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -2,10 +2,16 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "./server.ts";
 
+// TODO: Find a way to run this code only on client!
+// Since this file is imported on the server as well, the global js part
+// will simply be executed and will throw errors, bc. the window object on server does not have a location property.
+console.log("Origin: ", location.origin);
+const host = location?.origin;
+
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:8000/trpc",
+      url: `${host}/trpc`,
     }),
   ],
 });

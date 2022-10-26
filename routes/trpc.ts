0dca671c7d7e1e9b128707792d336145c16daad4
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 15:16:48 +0200

    add fetchRequestHandler of trpc to fresh /trpc endpoint

--- routes/trpc.ts
+++ routes/trpc.ts
@@ -1,18 +1,27 @@
 import { HandlerContext, Handlers } from "$fresh/server.ts";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "@/src/trpc/server.ts";
+import { createContext } from "@/src/trpc/fetch-context.ts";
 
 /**
  * NOTE: This route is currently not used in V1.0.0
  * TRPC Client is configured to directly connect to localhost:3000/trpc, which is the endpoint of the additional opine server
  */
 export const handler: Handlers = {
-  async GET(_req: Request, ctx: HandlerContext) {
-    const trpcReq = new Request("http://localhost:3000/trpc", _req);
-    const trpcResponse = await fetch(trpcReq);
-    return trpcResponse;
+  GET(_req: Request, ctx: HandlerContext) {
+    return fetchRequestHandler({
+      endpoint: "/trpc",
+      req: _req,
+      router: appRouter,
+      createContext,
+    });
   },
-  async POST(_req: Request, ctx: HandlerContext) {
-    const trpcReq = new Request("http://localhost:3000/trpc", _req);
-    const trpcResponse = await fetch(trpcReq);
-    return trpcResponse;
+  POST(_req: Request, ctx: HandlerContext) {
+    return fetchRequestHandler({
+      endpoint: "/trpc",
+      req: _req,
+      router: appRouter,
+      createContext,
+    });
   },
 };

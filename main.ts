f93e204755833337f9b45b8f1ebddea9da1a094c
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 15:19:17 +0200

    deactivate extra opine server

--- main.ts
+++ main.ts
@@ -8,9 +8,11 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import { opineServer } from "./src/trpc/opine-server.ts";
 
-const opinePort = 3000;
-opineServer.listen(opinePort, () => {
-  console.log(`TRPC V10 listening on Opine Server at port: ${opinePort}`);
-});
+// TODO: Cleanup when :8000/trpc route is working!
+// Deactivated for testing with trpc fetch adapter
+// const opinePort = 3000;
+// opineServer.listen(opinePort, () => {
+//   console.log(`TRPC V10 listening on Opine Server at port: ${opinePort}`);
+// });
 
 await start(manifest);

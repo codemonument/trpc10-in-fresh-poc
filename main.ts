841d98ff8a7a5313cbbca47a0152f3d7a3bb1905
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 18:43:54 +0200

    remove opine-server and trpc express adapter stuff

--- main.ts
+++ main.ts
@@ -6,13 +6,5 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
-import { opineServer } from "./src/trpc/opine-server.ts";
-
-// TODO: Cleanup when :8000/trpc route is working!
-// Deactivated for testing with trpc fetch adapter
-// const opinePort = 3000;
-// opineServer.listen(opinePort, () => {
-//   console.log(`TRPC V10 listening on Opine Server at port: ${opinePort}`);
-// });
 
 await start(manifest);

8c3719ee7015baa45625fc25e351057b5fa13fa8
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 14:11:05 +0200

    start a second server beside the fresh instance to run trpc

--- main.ts
+++ main.ts
@@ -6,5 +6,11 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import { opineServer } from "./src/trpc/opine-server.ts";
+
+const opinePort = 3000;
+opineServer.listen(opinePort, () => {
+  console.log(`TRPC V10 listening on Opine Server at port: ${opinePort}`);
+});
 
 await start(manifest);

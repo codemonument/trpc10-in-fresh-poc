97be049ef599cd6099530fb2ba7a40dd9e363ee2
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 14:28:51 +0200

    configure TRPC client completely route to the standalone trpc server

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -5,7 +5,7 @@ import type { AppRouter } from "./server.ts";
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:8000/trpc",
+      url: "http://localhost:3000/trpc",
     }),
   ],
 });

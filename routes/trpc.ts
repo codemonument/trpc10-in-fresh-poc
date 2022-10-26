227b26cdf32b9df03605c7b57b25321159ed942f
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 14:35:59 +0200

    add a changelog with better descriptions

--- routes/trpc.ts
+++ routes/trpc.ts
@@ -1,5 +1,9 @@
 import { HandlerContext, Handlers } from "$fresh/server.ts";
 
+/**
+ * NOTE: This route is currently not used in V1.0.0
+ * TRPC Client is configured to directly connect to localhost:3000/trpc, which is the endpoint of the additional opine server
+ */
 export const handler: Handlers = {
   async GET(_req: Request, ctx: HandlerContext) {
     const trpcReq = new Request("http://localhost:3000/trpc", _req);

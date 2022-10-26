6d44f09c98adc6df829a4332625799baca0dbf35
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 13:18:55 +0200

    fix fresh gen routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,19 +3,17 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/api/trpc.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $0 from "./routes/api/joke.ts";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/trpc.ts";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/TrpcPlayground.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/api/trpc.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/api/joke.ts": $0,
+    "./routes/index.tsx": $1,
+    "./routes/trpc.ts": $2,
   },
   islands: {
     "./islands/Counter.tsx": $$0,

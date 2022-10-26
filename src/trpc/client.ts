909683699a85a515e3b86fd94c4258e445fc612c
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 15:54:22 +0200

    add guard against not available 'location' variable

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -2,11 +2,15 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "./server.ts";
 
-// TODO: Find a way to run this code only on client!
-// Since this file is imported on the server as well, the global js part
-// will simply be executed and will throw errors, bc. the window object on server does not have a location property.
-console.log("Origin: ", location?.origin);
-const host = location?.origin;
+/**
+ * This guard check for the availability of 'location' is necessary,
+ * since location is not defined in global scope on server and crashes on deno deploy.
+ */
+let host;
+if (location) {
+  console.log("Origin: ", location?.origin);
+  host = location?.origin;
+}
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [

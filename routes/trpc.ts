6a72cb6f38a9784018db81c6d929e352e302f405
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 14:21:55 +0200

    try to setup a full opine server beside fresh server to handle trpc

--- routes/trpc.ts
+++ routes/trpc.ts
@@ -1,21 +1,14 @@
-import { HandlerContext } from "$fresh/server.ts";
-import { trpcExpressMiddleware } from "@/src/trpc/server.ts";
-import { resolve } from "https://deno.land/std@0.150.0/path/win32.ts";
+import { HandlerContext, Handlers } from "$fresh/server.ts";
 
-export const handler = async (
-  _req: Request,
-  _ctx: HandlerContext,
-): Promise<Response> => {
-  const expressHandlerPromise = await new Promise<Response>(
-    (resolve, reject) => {
-      trpcExpressMiddleware.apply(_req, {
-        // TODO: How to mock response object correctly?
-        send: (param: string) => {
-          resolve(new Response(param));
-        },
-      });
-    },
-  );
-
-  return expressHandlerPromise;
+export const handler: Handlers = {
+  async GET(_req: Request, ctx: HandlerContext) {
+    const trpcReq = new Request("http://localhost:3000/trpc", _req);
+    const trpcResponse = await fetch(trpcReq);
+    return trpcResponse;
+  },
+  async POST(_req: Request, ctx: HandlerContext) {
+    const trpcReq = new Request("http://localhost:3000/trpc", _req);
+    const trpcResponse = await fetch(trpcReq);
+    return trpcResponse;
+  },
 };

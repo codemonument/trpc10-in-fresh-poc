d8d26cbeccd5e185a274c48a4d5ceb66a66e290b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 14:08:32 +0200

    rename app to opineServer

--- src/trpc/opine-server.ts
+++ src/trpc/opine-server.ts
@@ -1,10 +1,10 @@
 import { opine } from "opine";
 import { trpcExpressMiddleware } from "@/src/trpc/server.ts";
 
-export const app = opine(); // opine is express ported to deno
+export const opineServer = opine(); // opine is express ported to deno
 
 // apply tRPC router as a middleware
-app.use(
+opineServer.use(
   "/trpc",
   trpcExpressMiddleware,
 );

125f02fbf1e74914378c8cbbf7e20dd3cb6256fa
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 15:50:38 +0200

    add missing elvis operator

--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -5,7 +5,7 @@ import type { AppRouter } from "./server.ts";
 // TODO: Find a way to run this code only on client!
 // Since this file is imported on the server as well, the global js part
 // will simply be executed and will throw errors, bc. the window object on server does not have a location property.
-console.log("Origin: ", location.origin);
+console.log("Origin: ", location?.origin);
 const host = location?.origin;
 
 export const trpc = createTRPCProxyClient<AppRouter>({

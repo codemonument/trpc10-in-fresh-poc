9bb426b7c8d90a55d8c8f88955c3dbf094a9e9b7
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-11-01 16:30:47 +0100

    remove unused expressMiddleware creation for trpc and simplify fetch context

--- src/trpc/fetch-context.ts
+++ src/trpc/fetch-context.ts
@@ -1,7 +1,10 @@
 import { inferAsyncReturnType } from "@trpc/server";
 import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+
 export function createContext({ req }: FetchCreateContextFnOptions) {
-  const user = { name: req.headers.get("username") ?? "anonymous" };
-  return { req, user };
+  //   const user = { name: req.headers.get("username") ?? "anonymous" };
+  //   return { req, user };
+  return { req };
 }
+
 export type Context = inferAsyncReturnType<typeof createContext>;
--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,11 +1,11 @@
 import { initTRPC } from "@trpc/server";
-import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { Context, createContext } from "./fetch-context.ts";
-// import { Context, createContext } from "./express-context.ts";
+import { Context } from "./fetch-context.ts";
 import { z } from "zod";
 
 const t = initTRPC.context<Context>().create();
 
+// This export is needed for integrating TRPC with Fresh in `/routes/trpc`
+// It is only used on the server!
 export const appRouter = t.router({
   hello: t.procedure
     .input(z.string())
@@ -14,12 +14,6 @@ export const appRouter = t.router({
     }),
 });
 
-// only export *type signature* of router!
-// to avoid accidentally importing your API
-// into client-side code
+// This export only export the *type signature* of the trpc router!
+// This avoids accidentally importing the full Router into client-side code
 export type AppRouter = typeof appRouter;
-
-export const trpcExpressMiddleware = createExpressMiddleware({
-  router: appRouter,
-  createContext,
-});
